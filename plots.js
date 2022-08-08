9ed71b4293c7e764703c738f851e9c74cfdb273b
Justin Papreck <justin.papreck@gmail.com>
2022-08-08 16:05:41 -0700

    Add and update files

--- plots.js
+++ plots.js
@@ -1,36 +1,39 @@
 function init() {
-    data = [{
-        x: [1, 2, 3, 4, 5],
-        y: [1, 2, 4, 8, 16]
-    }];
-    Plotly.newPlot("plot", data);
-};
+    var selector = d3.select("#selDataset");
 
-d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+        var sampleNames = data.names;
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+    })
+}
 
-function updatePlotly() {
-    var dropdownMenu = d3.select("#dropdownMenu");
-    var dataset = dropdownMenu.property("value");
+init();
 
-    var xData = [1, 2, 3, 4, 5];
-    var yData = [];
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
 
-    if (dataset === 'dataset1') {
-        yData = [1, 2, 4, 8, 16];
-    };
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
 
-    if (dataset === 'dataset2') {
-        yData = [1, 10, 100, 1000, 10000];
-    };
+        PANEL.html("");
+        PANEL.append("h6").text(result.location);
+    });
+}
 
-
-    var trace = {
-        x: [xData],
-        y: [yData]
-
-    };
-
-    Plotly.restyle("plot", trace);
-};
+// function buildCharts(sample) {
+// 
+// }
 
 init();
